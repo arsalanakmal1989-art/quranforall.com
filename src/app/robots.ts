0f01975105d58aff3d127bca63edf4a54a2d28d1
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2026-06-02 16:24:56 +0500

    style: update domain references to quranforalll.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://quranforall.com';
+  const baseUrl = 'https://quranforalll.com';
 
   return {
     rules: {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://quranforall.com';
+  const baseUrl = 'https://quranforalll.com';
   
   return [
     {
